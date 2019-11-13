77a9633b66ca5acd565ee258903302d46c57ccb9
Nealachito <nealdop@hotmail.es>
2019-11-13 09:04:00 -0500

    Commit Neal De Oro 13/11/2019

---
+++ src/main/java/tcs/group/prueba/NealSuma.java
@@ -0,0 +1,9 @@
+package tcs.group.prueba;
+
+public class NealSuma {
+	public float suma(float valor1, float valor2) {
+		float resultado=0;
+		resultado = valor1+valor2;
+		return resultado;
+	}
+}
